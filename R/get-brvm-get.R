f5a5b5b31fa6a0f25a5ccc3d505b8544684bf57e
Koffi Frederic Sessie <89697268+Fredysessie@users.noreply.github.com>
2022-05-19 06:09:32 +0000

    Update get-brvm-get.R

--- R/get-brvm-get.R
+++ R/get-brvm-get.R
@@ -37,92 +37,94 @@
 #' @export
 #'
 
-BRVM_get <- function(.symbol, .from = NULL, .to = NULL, .turn_off_warnings = TRUE) {
-
-    # Evaluate input parameters ----
-    tickers <- unique(toupper(.symbol))
-    start_date <- lubridate::parse_date_time(.from, orders = "ymd")
-    end_date <- lubridate::parse_date_time(.to, orders = "ymd")
-    turn_off_warnings <- as.logical(.turn_off_warnings)
-
-    # Check input parameters ----
-    if (length(end_date) == 0){
-        end_date <- lubridate::ymd(Sys.Date())
+BRVM_get <- function(.symbol, .from = Sys.Date() - 365, .to = Sys.Date() - 1) {
+  # Evaluate input parameters ----
+  tickers <- unique(toupper(.symbol))
+  Symbole <- c( "ABJC", "BICC", "BNBC", "BOAB", "BOABF", "BOAC", "BOAM", "BOAN", "BOAS", "CABC", "CBIBF", "CFAC", "CIEC", "ECOC", "ETIT", "FTSC", "NEIC", "NSBC", "NTLC", "ONTBF", "ORGT", "PALC", "PRSC", "SAFC", "SCRC", "SDCC", "SDSC", "SEMC", "SGBC", "SHEC", "SIBC", "SICC", "SIVC", "SLBC", "SMBC", "SNTS", "SOGC", "SPHC", "STAC", "STBC", "SVOC", "TTLC", "TTLS", "UNLC", "UNXC"
+                #, "TTRC"
+  )  
+  ifelse(tickers=="ALL", 
+         tickers<-Symbole,
+         tickers)
+  start_date <-  lubridate::parse_date_time(.from, orders = "ymd")
+  end_date <- lubridate::parse_date_time(.to, orders = "ymd")
+  # Check input parameters ----
+  if (length(tickers) < 1){
+    rlang::abort(
+      "The '.symbol' parameter cannot be blank. Please enter at least one ticker.
+            If entering multiple please use .symbol = c(Tick_1, Tick_2, ...)"
+    )
+  }
+  
+  if (start_date > end_date){
+    rlang::abort(
+      "The '.from' parameter (start_date) must be equal to or less than .to (end_date)"
+    )
+  }
+  
+  returns <- as.data.frame(matrix(NA, ncol = 7, nrow = 0))
+  names(returns) <- c("Date", "Open", "High", "Low", "Close", "Volume", "Ticker")
+  
+  #### Create a definitive symbol vector
+  symbol_vec <- NULL
+  ## Filter symbol in quote symbol list
+  for (symb in tickers) {
+    if (symb %in% Symbole) {
+      symbol_vec <- c(symbol_vec, symb)
     }
-
-    if (length(tickers) < 1){
-        rlang::abort(
-            message = "The '.symbol' parameter cannot be blank. Please enter at least one ticker.
-            If entering multiple please use .symbol = c(Tick_1, Tick_2, ...)",
-            use_cli_format = TRUE
-        )
-    }
-
-
-    if (inherits(start_date, "Date") & inherits(end_date, "Date") &
-        length(start_date) != 0 &
-        length(end_date) != 0){
-        if(start_date > end_date){
-            rlang::abort(
-                message = "The '.from' parameter (start_date) must be equal to or less than .to (end_date)",
-                use_cli_format = TRUE
-            )
-        }
-    }
-
-    if (!is.logical(turn_off_warnings)){
-        rlang::warn(
-            message = paste0("A non-boolean value was passed to '.turn_off_warnings',
-                             it will be set to TRUE. The value passed is: ", turn_of_warnings),
-            use_cli_format = TRUE
-        )
-        turn_off_warnings <- TRUE
-    } else {
-        turn_off_warnings <- turn_off_warnings
-    }
-
-    if (turn_off_warnings){
-        options(warn = -1)
-    }
-
-    returns <- as.data.frame(matrix(NA, ncol = 7, nrow = 0))
-    names(returns) <- c("Date", "Open", "High", "Low", "Close", "Volume", "Ticker")
-    quotes <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1rdjGjlQg7cUzWAEJFikrxOnisk-yQQx-n652sJUL-qc/edit#gid=0")
-    colnames(quotes)<-c("Symbole","Nom","Volume","Cours veille (FCFA)","Cours clôture (FCFA)" ,"Cours Clôture(FCFA)","Variation(%)" )
-
-    #### Create a definitive symbol vector
-    symbol_vec <- NULL
-    ## Filter symbol in quote symbol list
-    for (symb in tickers) {
-        if (symb %in% quotes$Symbole) {
-            symbol_vec <- append(symbol_vec, symb)
-        }
-    }
-    # Check input parameters after filtering ----
-    if (length(symbol_vec) < 1){
-        rlang::abort(
-            message = "The '.symbol' parameter cannot be blank. Please enter at least one ticker.
-            If entering multiple please use .symbol = c(Tick_1, Tick_2, ...)",
-            use_cli_format = TRUE
-        )
-    }
-    for (Tick in symbol_vec) {
+  }
+  # Check input parameters after filtering ----
+  if (length(symbol_vec) < 1){
+    rlang::abort(
+      "The '.symbol' parameter cannot be blank. Please enter at least one ticker.
+            If entering multiple please use .symbol = c(Tick_1, Tick_2, ...)"
+    )
+  }
+  tryCatch(
+    {
+      for (Tick in symbol_vec) {
         url <- paste0("https://www.richbourse.com/common/mouvements/technique/", Tick, "/status/200")
         Sys.sleep(1)
         page <- httr::GET(url)
         page <- httr::content(page, as = "text", encoding = "UTF-8")
         page <- unlist(strsplit(page, split = "\n"))
-        data1 <- unlist(strsplit(page[[642]], split = ":"))
+        vect.data<- NULL 
+        
+        for (i in 600:650){
+          if (length(unlist(strsplit(page[[i]], split = ":")))==2) {
+            if ((unlist(strsplit(page[[i]], split = ":")))[[1]] == "                data") {
+              vect.data<- c(vect.data, i)
+            }
+            
+          }
+          
+        }
+        # vect.data[1]   #is the historical data 
+        # vect.data[2]  # is the volume data
+        if (length(vect.data) < 2) {
+          vect.data<- NULL 
+          for (i in 550:700){
+            if (length(unlist(strsplit(page[[i]], split = ":")))==2) {
+              if ((unlist(strsplit(page[[i]], split = ":")))[[1]] == "                data") {
+                vect.data<- c(vect.data, i)
+              }
+              
+            }
+            
+          }
+        }
+        
+        data1 <- unlist(strsplit(page[[vect.data[1]]], split = ":"))
         data1 <- data1[2] # Show table 1 ##First 5 columns (Date, Open, High, Low, Close)
         data1 <- gsub(" ", "", data1)
         data1 <- strsplit(data1, split = "],")
         data1 <- as.data.frame(data1)
-        i <- 1
+        
         for (i in 1:nrow(data1)) {
-            data1[i, 1] <- gsub("\\[|\\]", "", data1[i, 1])
+          data1[i, 1] <- gsub("\\[|\\]", "", data1[i, 1])
         }
         ### Now transform one column to 5 columns
-
+        
         ## And change numbers in integer
         colnames(data1) <- c("unique")
         data1 <- tidyr::separate(data1, col = unique, into = c("Date", "Open", "High", "Low", "Close"), sep = ",")
@@ -133,63 +135,65 @@ BRVM_get <- function(.symbol, .from = NULL, .to = NULL, .turn_off_warnings = TRU
         # Transform date from character to numeric
         data1$Date <- as.numeric(data1$Date)
         ## Turn date in format "%Y-%m-%d"
-        data1$Date <- as.Date(as.POSIXct((data1$Date + 0.1) / 1000, origin = "1970-01-01"))
-
+        #data1$Date <- as.Date(as.POSIXct((data1$Date + 0.1) / 1000, origin = "1970-01-01"))
+        data1$Date <- as.Date.POSIXct((data1$Date + 0.1) / 1000)
+        
         ## Volume data Case (2 columns : Date and Volume)
-        data2 <- unlist(strsplit(page[[652]], split = ":"))
-        data2 <- data2[2]
+        data2 <- unlist(strsplit(page[[vect.data[2]]], split = ":"))
+        data2 <- data2[[2]]
         # Data Cleaning
         data2 <- gsub(" ", "", data2) # Volume
         ### Remove all second square bracket closer to a comma
         data2 <- strsplit(data2, split = "],") # Volume
         data2 <- as.data.frame(data2) # Volume
         ### Use a loop to remove the remaining square brackets in each row
-        j <- 1
+        #j <- 1
+        Date=NULL
+        Volume=NULL
         for (j in 1:nrow(data2)) {
-            data2[j, 1] <- gsub("\\[|\\]", "", data2[j, 1])
+          data2[j, 1] <- gsub("\\[|\\]", "", data2[j, 1])
+          Date <- c(Date,unlist(strsplit(data2[j, 1],split = ','))[1])
+          Volume <- c(Volume,unlist(strsplit(data2[j, 1],split = ','))[[2]])
         }
-        ### the following code ("separate function") return warning message but the result is good
-        # How can we hide the warning ?    colnames(data2)<-c("unique")
-        colnames(data2) <- "unique"
-        data2 <- tidyr::separate(data2, col = unique, into = c("Date", "Volume", sep = ","))[1:2]
-        Sys.sleep(1)
-        data2$Volume <- as.numeric(data2$Volume)
-        data2$Date <- as.numeric(data2$Date)
+        
+        #Sys.sleep(1)
+        Volume <- as.numeric(Volume)
+        Date <- as.Date.POSIXct((as.numeric(Date) + 0.1) / 1000)
+        data2 <- data.frame(Date,Volume)
         ## Turn date in format "%Y-%m-%d"
-        data2$Date <- as.Date(as.POSIXct((data2$Date + 0.1) / 1000, origin = "1970-01-01"))
+        #data2$Date <- as.Date(as.POSIXct((data2$Date + 0.1) / 1000, origin = "1970-01-01"))
         ## Join data by date
-        final.data <- dplyr::left_join(data1, data2, by = "Date")
-        ###Group data by date if they exist
+        final.data <- merge(data1, data2, by = "Date")
         ifelse (any(duplicated(final.data$Date)),
-            final.data<-final.data%>%
-                group_by(Date)%>%
-                summarise(Open=ceiling(mean(Open)),
-                          High= ceiling(mean(High)),
-                          Low= ceiling(mean(Low)),
-                          Close= ceiling(mean(Close)),
-                          Volume= ceiling(mean(Volume))),
-             final.data)
-
+                final.data<-final.data%>%
+                  dplyr::group_by(Date)%>%
+                  summarise(Open=ceiling(mean(Open)),
+                            High= ceiling(mean(High)),
+                            Low= ceiling(mean(Low)),
+                            Close= ceiling(mean(Close)),
+                            Volume= ceiling(mean(Volume))),
+                final.data)
+        assign(Tick, dplyr::as_tibble(final.data),envir = globalenv())
         final.data$Ticker <- Tick ## Add ticker identifier
-        assign(Tick, dplyr::as_tibble(final.data))
         returns <- rbind(returns, final.data)
+      }
+      returns <- dplyr::as_tibble(returns) %>%
+        dplyr::filter(Date >= start_date) %>%
+        dplyr::filter(Date <= end_date)
+      
+      if (length(unique(returns$Ticker)) > 1) {
+        returns <- returns
+      } else {
+        returns<- returns[, -7]
+      }
+      print(symbol_vec)
+    },
+    error = function(e) {
+      print("Make sure you have an active internet connection")
+    },
+    warning = function(w) {
+      print("Make sure you have an active internet connection")
     }
-
-    # Set column names ----
-    returns <- dplyr::as_tibble(returns) %>%
-        purrr::set_names("date_col", "open","high","low","close","volume","ticker") %>%
-        dplyr::select(date_col, ticker, dplyr::everything())
-
-    # Time filtering ----
-    if (length(start_date) != 0){
-        returns <- dplyr::filter(returns, date_col >= start_date)
-    }
-
-    if (length(end_date) != 0){
-        returns <- dplyr::filter(returns, date_col <= end_date)
-    }
-
-    # Return ----
-    options(warn = 0)
-    return(returns)
+  )
+  return(returns)
 }
