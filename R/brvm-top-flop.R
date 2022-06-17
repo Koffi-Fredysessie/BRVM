f9d036878c17f77523e34e2cce720c5d5fd226b6
Koffi Frederic Sessie <89697268+Fredysessie@users.noreply.github.com>
2022-06-17 01:37:15 +0000

    Update brvm-top-flop.R

--- R/brvm-top-flop.R
+++ R/brvm-top-flop.R
@@ -1,81 +1,71 @@
-#' BRVM Rank
+#' BRVM_rank - Get top or flop data of BRVM stock exchange 
 #'
-#' @family Ranking
-#' @family Ticker Data Retrieval
+#' @param top_or_flop Choose between "top" or "flop"
+#' @param n is the number of companies in the classification
+#'
+#' @return "tbl_df"     "tbl"        "data.frame"
+#'
+#' @description It receives "top" or "flop" and a number 'n' and
+#' returns table of companies classification 
 #'
 #' @author Koffi Frederic SESSIE
 #' @author Oudouss Diakité Abdoul
 #' @author Steven P. Sanderson II, MPH
 #'
-#' @details This function will get any n results for the top or flop ranking
-#' of the BRVM tickers.
-#'
-#' @description
-#'
-#' @param .top_or_flop This is a character string set to "Top". It can either
-#' be "Top" or "Flop".
-#' @param .n How many records to return
-#'
 #' @examples
-#'
-#' BRVM_rank("Top", 1)
-#' BRVM_rank("blahblah", 1)
-#' BRVM_rank("Flop", 1)
-#'
-#' @return
-#' A tibble
-#'
+#' BRVM_rank("top", 15)
+#' BRVM_rank("flop", 5)
 #' @export
-#'
 
-BRVM_rank <- function(.top_or_flop = "Top", .n = 10) {
-
-    # Set params ----
-    top_flop <- tolower(as.character(.top_or_flop))
-    n <- as.integer(.n)
-
-    # Check params ----
-    if (!is.character(top_flop)){
-        rlang::abort(
-            "The '.top_or_flop' parameters must be set to either top or flop."
-        )
-    }
-
-    if (!is.numeric(n)){
-        rlang::abort(
-            "The '.n' parameter must be an numeric integer like 1 or 10."
-        )
-    }
-
-    # get data ----
-    quotes_tbl <- gsheet::gsheet2tbl(" https://docs.google.com/spreadsheets/d/1rdjGjlQg7cUzWAEJFikrxOnisk-yQQx-n652sJUL-qc/edit#gid=0 ")
-    quotes_tbl$`Variation (%)` <- gsub(",", ".", quotes_tbl$`Variation (%)`)
-    quotes_tbl$`Variation (%)` <- as.numeric(quotes_tbl$`Variation (%)`)
-    quotes_tbl <- quotes_tbl[-c(3:6)]
-
-    # Get nrow of quoats, if n > nrow(quotes_tbl) then error out ----
-    if(nrow(quotes_tbl) < n){
-        rlang::abort(
-            "You have chosen more records than exist in the Quotes Tibble."
-        )
-    }
-
-    if (top_flop == "top") {
-        ret <- dplyr::arrange(quotes_tbl, dplyr::desc(quotes_tbl$`Variation (%)`)) %>%
-            dplyr::slice(1:n)
-        ret <- dplyr::as_tibble(ret)
-        #assign(paste0("Top", "_", n), quotes_tbl[1:n, ])
-    } else if (top_flop == "flop") {
-        quotes_tbl$rank <- rank(quotes_tbl$`Variation (%)`)
-        quotes_tbl <- quotes_tbl[order(quotes_tbl$rank), ]
-        ret <- quotes_tbl %>%
-            dplyr::slice(1:n) %>%
-            dplyr::select(-rank)
-        ret <- dplyr::as_tibble(ret)
-        #assign(paste0("Flop", "_", n), quotes_tbl[-4][1:n, ])
-    }
-
-    # Return data ----
-    return(ret)
-
+BRVM_rank<-function(top_or_flop, n=10){
+  if (n > 46) {
+    print("Please choose a number lower than or equal to 46")
+    # return("Please choose a number lower than or equal to 46")
+  } else if (str_to_title(top_or_flop) !="Top" && str_to_title(top_or_flop) !="Flop"){
+    print("Please choose between Top and Flop")
+    # return("Please choose between Top and Flop")
+    
+  } else {
+    tryCatch(
+      {
+        # quotes.df = gsheet::gsheet2tbl(" https://docs.google.com/spreadsheets/d/1rdjGjlQg7cUzWAEJFikrxOnisk-yQQx-n652sJUL-qc/edit#gid=0 ")
+        quotes.df <- rvest::read_html("https://www.brvm.org/en/cours-actions/0/status/200") %>%
+          rvest::html_nodes('table') %>%
+          rvest::html_table()
+        quotes.df <- quotes.df[[4]]    #[1:2]
+        quotes.df <- tibble::as.tibble(quotes.df)
+        quotes.df$`Change (%)`<-gsub(",", ".", quotes.df$`Change (%)`)
+        quotes.df$`Change (%)`<-as.numeric(quotes.df$`Change (%)`)
+        quotes.df <-quotes.df[-c(3:6)]
+        names(quotes.df) <- c("Ticker", "Name", "Change (%)")
+        
+        # if (str_to_title(top_or_flop) %in% c("Top","Flop")){
+          if (str_to_title(top_or_flop) == "Top"){
+            quotes.df<-arrange(quotes.df, desc(quotes.df$`Change (%)`))
+            assign(paste0("Top","_", n), quotes.df[1:n,])
+            quotes.df <- tibble::as.tibble(quotes.df[1:n,])
+            return(quotes.df)
+            
+          } else if (str_to_title(top_or_flop) == "Flop"){
+            quotes.df$rank<-rank(quotes.df$`Change (%)`)
+            quotes.df<- quotes.df[order(quotes.df$rank),]
+            assign(paste0("Flop","_", n), quotes.df[-4][1:n,])
+            quotes.df <- tibble::as.tibble(quotes.df[-4][1:n,])
+            return(quotes.df)
+          }   else {
+            print("BRVM_rank('Top', 5) or BRVM_rank('Flop', 5)")
+          }
+          
+          
+        # } 
+        
+      },
+      error = function(e) {
+        print("Make sure you have an active internet connection")
+      },
+      warning = function(w) {
+        print("Make sure you have an active internet connection")
+      }
+    )
+  }
 }
