ab01e51a42c4280d5087d201e0d337b55b7c3568
Koffi Frederic Sessie <89697268+Fredysessie@users.noreply.github.com>
2022-06-20 15:23:04 +0000

    Update brvm-stationarity-test.R

--- R/brvm-stationarity-test.R
+++ R/brvm-stationarity-test.R
@@ -14,10 +14,10 @@
 #' @return a number that indicates the P-value of the stationarity test 
 #' @export
 #'
-#' @examples
+#' @examples \dontrun{
 #' #' y <-ts(c(5353.08,5409.24,5315.57,5270.53, # one and a half week stock index
 #' 5211.66,NA,NA,5160.80,5172.37))  # data including a weekend
-#' stationarity_test(y, "Box-Pierce and Ljung-Box")
+#' stationarity_test(y, "Box-Pierce and Ljung-Box")}
 #' 
 stationarity_test <- function(x, type.test) {
   if (length(unique(x)) != 1) {
