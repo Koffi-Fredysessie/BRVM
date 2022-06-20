2bf8730ffbe36ac8323c32dafa1bc3e04738911e
Koffi Frederic Sessie <89697268+Fredysessie@users.noreply.github.com>
2022-06-20 14:00:57 +0000

    Create brvm-allindex.R

---
+++ R/brvm-allindex.R
@@ -0,0 +1,22 @@
+#' BRVM Available Indexes
+#'
+#' @description It receives no argument and returns BRVM's indexes informations.
+#'
+#' @family BRVM
+#' 
+#' @author Koffi Frederic SESSIE
+#' 
+#' @return
+#' A character
+#' 
+#' @export
+#' 
+#' @examples
+#' BRVM.index()
+#' 
+
+BRVM.index <- function(){
+  all_tick <- c("BRVM10", "BRVMAG", "BRVMC", "BRVMAS", "BRVMDI", "BRVMFI", 
+                "BRVMIN", "BRVMSP", "BRVMTR")
+  return(all_tick)
+}
