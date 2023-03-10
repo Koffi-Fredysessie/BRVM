70451f36bbf6035384cce78562417dcb4676fc2a
Koffi Frederic Sessie <89697268+Fredysessie@users.noreply.github.com>
2023-03-10 19:58:54 +0000

    Update 00_global_variables.R

--- R/00_global_variables.R
+++ R/00_global_variables.R
@@ -1,12 +1,12 @@
 globalVariables(
-    names = c('.indexes', '.sectors', 'Box.test', 'Close', 'Countries', 'Date', 'High', 'Low', 'Open', 'POST',
-              'Sector', 'Ticker', 'Volume', 'ad.test', 'adf.test', 'arrange', 'as.xts', 'color_tile',
+    names = c('.indexes', 'adn', '.sectors', 'Box.test', 'Close', 'Countries', 'Date', 'High', 'Low', 'Open', 'POST',
+              'Sector', 'Ticker', 'Desc.BRVM.all', 'Volume', 'ad.test', 'adf.test', 'arrange', 'as.xts', 'color_tile',
               'content', 'dagoTest', 'desc', 'direction', 'formattable', 'formatter', 'global_env',
               'hc_add_series', 'hc_add_yAxis', 'hc_colors', 'hc_exporting', 'hc_title', 'hc_xAxis',
               'hc_yAxis_multiples', 'hcaes', 'highchart', 'html_nodes', 'html_table', 'is.ts',
               'isWeekday', 'jarque.bera.test', 'kpss.test', 'left_join', 'lillie.test', 'na.omit',
               'na.remove', 'pearson.test', 'pp.test', 'read_html', 'req_body_json', 'req_perform',
               'request', 'resp_body_json', 'sf.test', 'shapiro.test', 'str_replace', 'str_sub',
-              'str_to_title', 'summarise', "html_elements"
+              'str_to_title', 'summarise', 'html_elements'
               )
     )
