73bcc675f91579dac72267848a769692c0d2f033
Koffi-Fredysessie <koffisessie@gmail.com>
2023-04-20 11:15:47 +0000

    update

--- R/brvm-stationarity-test.R
+++ R/brvm-stationarity-test.R
@@ -15,6 +15,7 @@
 #' @export
 #'
 #' @examples
+#'library(tseries)
 #'
 #' #' y <-ts(c(5353.08,5409.24,5315.57,5270.53, # one and a half week stock index
 #' 5211.66,NA,NA,5160.80,5172.37))  # data including a weekend
