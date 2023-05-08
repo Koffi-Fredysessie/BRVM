87799a4b681d0f677a619a9be44750093983b351
Koffi-Fredysessie <koffisessie@gmail.com>
2023-05-08 15:51:04 +0000

    update

--- R/brvm-company-cap.R
+++ R/brvm-company-cap.R
@@ -25,27 +25,43 @@
 #' @export
 
 company_cap <- function(company){
-  company<-toupper(company)
-  tryCatch({
-    brvm_cap_all <- rvest::read_html("https://www.brvm.org/en/capitalisations/0/status/200") %>%
-      rvest::html_elements('table') %>%
-      rvest::html_table()
-    brvm_cap_all <- brvm_cap_all[[4]]
-    brvm_cap_all$`Global capitalization (%)`<-gsub(",", ".",brvm_cap_all$`Global capitalization (%)`)
-    for (elm in 1:nrow(brvm_cap_all)){
-      if (brvm_cap_all[elm,1]==company){
-        the_cap<-paste0(brvm_cap_all[elm,6], "")
-      }
+    company<-toupper(company)
+
+    Symbole <- c("ABJC", "BICC", "BNBC", "BOAB", "BOABF", "BOAC",
+                 "BOAM", "BOAN", "BOAS", "CABC", "CBIBF", "CFAC",
+                 "CIEC", "ECOC", "ETIT", "FTSC", "NEIC", "NSBC",
+                 "NTLC", "ONTBF", "ORGT", "ORAC","PALC", "PRSC",
+                 "SAFC", "SCRC", "SDCC", "SDSC", "SEMC", "SGBC",
+                 "SHEC", "SIBC", "SICC", "SIVC", "SLBC", "SMBC",
+                 "SNTS", "SOGC", "SPHC", "STAC", "STBC", "SVOC",
+                 "TTLC", "TTLS", "UNLC", "UNXC"
+                 #, "TTRC"
+    )
+
+    if (company %in% Symbole){
+        tryCatch({
+            brvm_cap_all <- rvest::read_html("https://www.brvm.org/en/capitalisations/0/status/200") %>%
+                rvest::html_elements('table') %>%
+                rvest::html_table()
+            brvm_cap_all <- brvm_cap_all[[4]]
+            brvm_cap_all$`Global capitalization (%)`<-gsub(",", ".",brvm_cap_all$`Global capitalization (%)`)
+            for (elm in 1:nrow(brvm_cap_all)){
+                if (brvm_cap_all[elm,1]==company){
+                    the_cap<-paste0(brvm_cap_all[elm,6], "")
+                }
+            }
+
+            return(the_cap)
+
+        },
+        error = function(e) {
+            message("Make sure you have an active internet connection")
+        },
+        warning = function(w) {
+            message("Make sure you have an active internet connection")
+        })
+
+    } else{
+        message(paste0("Make sure ", company, " is BRVM Ticker"))
     }
-
-    return(the_cap)
-
-  },
-  error = function(e) {
-    print("Make sure you have an active internet connection")
-  },
-  warning = function(w) {
-    print("Make sure you have an active internet connection")
-  })
 }
-
